f546e76069e6b67463ae7caf3a1cb2da05952c47
maudetes <maudet.estelle@gmail.com>
2022-07-26 11:21:02 +0200

    Update message schema and consume functions logic

--- udata_event_consumer/dataset/event.py
+++ udata_event_consumer/dataset/event.py
@@ -10,14 +10,14 @@ def concat_location(location: dict) -> str:
     Return an url from an object storage location object.
     Input example:
     {
-        "url": "http://minio:9000/",
+        "netloc": "http://minio:9000/",
         "bucket": "udata",
         "key": "data/71cacba9-4ffd-4ea8-bc3e-694425ddf006"
     }
     would return the following string:
     "http://minio:9000/udata/data/71cacba9-4ffd-4ea8-bc3e-694425ddf006"
     '''
-    return '/'.join([location['url'].strip('/'), location['bucket'], location['key']])
+    return '/'.join([location['netloc'].strip('/'), location['bucket'], location['key']])
 
 
 def consume_message_resource_analysed(key: str, message: dict) -> None:
@@ -28,19 +28,19 @@ def consume_message_resource_analysed(key: str, message: dict) -> None:
     log.info('Consuming message analysed')
     resource = get_resource(UUID(key))
     value = message['value']
+    service = message["service"]
     if resource:
-        if message['service'] == 'udata-hydra':
-            resource.extras['analysis:error'] = value['error']
-            resource.extras['analysis:filesize'] = value['filesize']
-            resource.extras['analysis:mime'] = value['mime']
-        elif message['service'] == 'detective':
-            resource.extras['analysis:data_location'] = concat_location(value['data_location'])
+        if service == 'udata-hydra':
+            for key in ['error', 'filesize', 'mime']:
+                if value[key]:
+                    resource.extras[f'analysis:{key}'] = value[key]
+        elif service == 'csvdetective':
             resource.extras['analysis:report_location'] = concat_location(value['report_location'])
-            resource.extras['analysis:schema_location'] = concat_location(value['schema_location'])
+            resource.extras['analysis:tableschema_location'] = concat_location(value['tableschema_location'])
             resource.extras['analysis:encoding'] = value['encoding']
             resource.extras['analysis:delimiter'] = value['delimiter']
         else:
-            log.warn(f'Unexpected service for resource analysed topic for key {key}')
+            log.warn(f'Unexpected service {service} for resource analysed topic for key {key}')
         resource.save()
     else:
         log.warn(f'No resource found for key {key}')
@@ -53,7 +53,7 @@ def consume_message_resource_stored(key: str, message: dict) -> None:
     log.info('Consuming message stored')
     resource = get_resource(UUID(key))
     if resource:
-        resource.extras['store:location'] = concat_location(message['value']['data_location'])
+        resource.extras['store:data_location'] = concat_location(message['value']['data_location'])
         resource.save()
     else:
         log.warn(f'No resource found for key {key}')
@@ -68,9 +68,9 @@ def consume_message_resource_checked(key: str, message: dict) -> None:
     resource = get_resource(UUID(key))
     value = message['value']
     if resource:
-        resource.extras['check:status'] = value['status']
-        resource.extras['check:timeout'] = value['timeout']
-        resource.extras['check:check_date'] = value['check_date']
+        for key in ['status', 'timeout', 'check_date']:
+            if value[key]:
+                resource.extras[f'check:{key}'] = value[key]
         resource.save()
     else:
         log.warn(f'No resource found for key {key}')
